20106f988d9c727f205f715030654a6041d8cc7d
Ari Chivukula <ari@chivuku.la>
2019-01-01 13:52:55 -0800

    Base Tiny Project

---
+++ stroke/TinyExport/Our Forbidden Country/html5/development/bin/entities.js
@@ -0,0 +1,37 @@
+﻿/**
+ * TINY GENERATED CODE, DO NOT EDIT BY HAND
+ * @project Our Forbidden Country
+ */
+
+entities.game.NewEntityGroup.name = "NewEntityGroup";
+entities.game.NewEntityGroup.load = function(world) {
+    var arch0 = world.createArchetype(this.Component, ut.Core2D.Camera2D, ut.Core2D.TransformNode, ut.EditorExtensions.CameraCullingMask, ut.layers.Default)
+    var e0 = world.createEntity(arch0);
+    world.setEntityName(e0, "Camera");
+    var c0 = new ut.Core2D.TransformNode();
+    world.setComponentData(e0, c0);
+    var c1 = new ut.Core2D.Camera2D();
+    c1.halfVerticalSize = 5;
+    var s0 = new ut.Math.Rect();
+    s0.x = 0;
+    s0.y = 0;
+    s0.width = 1;
+    s0.height = 1;
+    c1.rect = s0;
+    var s1 = new ut.Core2D.Color();
+    s1.r = 0;
+    s1.g = 0;
+    s1.b = 0;
+    s1.a = 1;
+    c1.backgroundColor = s1;
+    c1.clearFlags = 1;
+    c1.depth = -1;
+    c1.cullingMask = [ut.layers.Default.cid, ut.layers.TransparentFX.cid, ut.layers.IgnoreRaycast.cid, ut.layers.Water.cid, ut.layers.UI.cid];
+    c1.cullingMode = 2;
+    world.setComponentData(e0, c1);
+    var c2 = new ut.EditorExtensions.CameraCullingMask();
+    c2.mask = -1;
+    world.setComponentData(e0, c2);
+    return [e0];
+}
+
